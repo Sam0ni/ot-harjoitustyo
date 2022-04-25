e985e962466087ff0dee28afc646293df003d1ea
Samppa <samppasalo16@gmail.com>
2022-04-25 23:40:00 +0300

    add new tests

--- spaceinvaders/src/tests/spaceinv_test.py
+++ spaceinvaders/src/tests/spaceinv_test.py
@@ -1,10 +1,13 @@
 import unittest
 from space.space import Space
+from menu.menu import Menu
+from sprites.item import Item
 
 
 class TestLevel(unittest.TestCase):
     def setUp(self):
         self.map = Space(1280, 720)
+        self.menu = Menu()
 
     def assert_coordinates_equal(self, sprite, x, y):
         self.assertEqual(sprite.rect.x, x)
@@ -59,3 +62,58 @@ class TestLevel(unittest.TestCase):
         self.map.move_pellets(0, -600)
         
         self.assert_amount_in_group_equal(self.map.invaders, 63)
+
+    def test_menu_start(self):
+        press_go = self.menu.go_there()
+
+        self.assertEqual(press_go, True)
+
+    def test_menu_quit(self):
+        self.menu.cursor = 3
+        press_go = self.menu.go_there()
+
+        self.assertEqual(press_go, False)
+
+    def test_menu_highscore(self):
+        self.menu.cursor = 2
+        press_go = self.menu.go_there()
+
+        self.assertEqual(press_go, "highscores")
+
+    def test_items_can_spawn(self):
+        self.map.items.add(Item(20, 40))
+        self.assertEqual(len(self.map.items), 1)
+
+    def test_items_can_move(self):
+        self.map.items.add(Item(20, 40))
+        self.map.move_items(0, 20)
+        for item in self.map.items:
+            self.assertEqual(item.rect.y, 60)
+
+    def test_menu_cursor_up(self):
+        self.menu.move_cursor_up()
+        self.assertEqual(self.menu.cursor, 3)
+        self.assertEqual(self.menu.start_color, (0,0,255))
+        self.assertEqual(self.menu.quit_color, (255,0,0))
+        self.menu.move_cursor_up()
+        self.assertEqual(self.menu.cursor, 2)
+        self.assertEqual(self.menu.quit_color, (0,0,255))
+        self.assertEqual(self.menu.scores_color, (255,0,0))
+        self.menu.move_cursor_up()
+        self.assertEqual(self.menu.cursor, 1)
+        self.assertEqual(self.menu.scores_color, (0,0,255))
+        self.assertEqual(self.menu.start_color, (255,0,0))
+
+    def test_menu_cursor_down(self):
+        self.menu.move_cursor_down()
+        self.assertEqual(self.menu.cursor, 2)
+        self.assertEqual(self.menu.start_color, (0,0,255))
+        self.assertEqual(self.menu.scores_color, (255,0,0))
+        self.menu.move_cursor_down()
+        self.assertEqual(self.menu.cursor, 3)
+        self.assertEqual(self.menu.scores_color, (0,0,255))
+        self.assertEqual(self.menu.quit_color, (255,0,0))
+        self.menu.move_cursor_down()
+        self.assertEqual(self.menu.cursor, 1)
+        self.assertEqual(self.menu.quit_color, (0,0,255))
+        self.assertEqual(self.menu.start_color, (255,0,0))
